b470fb9ef46f276c74bb9e18a1c1ca54a3ff66a3
Branden Cash <bcash@parchment.com>
2015-11-06 21:29:12 -0700

    allow customizing the user id and group id

--- couchpotato.sh
+++ couchpotato.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 umask 000
 
+usermod -u $USER_ID nobody
+usermod -g $GROUP_ID nobody
 chown -R nobody:users /opt/couchpotato /config
 
 exec /sbin/setuser nobody python /opt/couchpotato/CouchPotato.py --config_file=/config/config.ini --data_dir=/config/data
